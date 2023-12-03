5e24b493c3530695925943f861597402f005dd7b
Sebanti2003 <dasguptasebanti2003@gmail.com>
2023-12-03 16:16:06 +0530

    connected to db

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
-import express from 'express';
-const app=express();
-app.get('/',(req,res)=>{
-    res.json([{name:"Sebanti",age:28},{name:"Sebanti",age:28},{name:"Sebanti",age:28}])
-})
-app.listen(5000,()=>{
-    console.log(`Server is running on port ${5000}`);
-})
+import express from "express";
+import mongoose, { mongo } from "mongoose";
+import dotenv from "dotenv";
+const app = express();
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("Connected to Mongo Database"))
+  .catch((error) => console.log(error," is the Error"));
+app.get("/", (req, res) => {
+  res.json([
+    { name: "Sebanti", age: 28 },
+    { name: "Sebanti", age: 28 },
+    { name: "Sebanti", age: 28 },
+  ]);
+});
+app.listen(5000, () => {
+  console.log(`Server is running on port ${5000}`);
+});
